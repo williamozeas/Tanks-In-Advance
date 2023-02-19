f38bfcb2a2c56b37e25a92dda91e6f44d6f1e9eb
RandomAccount1128 <zheintzleman@gmail.com>
2023-02-19 15:18:18 -0500

    Update BreakableWall.cs

--- Tanks In Advance/Assets/Scripts/Objects/Buildings/BreakableWall.cs
+++ Tanks In Advance/Assets/Scripts/Objects/Buildings/BreakableWall.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BreakableWall : Wall
 {
     public int health;
-    public const int MaxHealth = 10;
+    public const int MaxHealth = 3;
 
     // Start is called before the first frame update
     void Start()
