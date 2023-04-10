fdd0f6764a991694fb831fb24598b0de5a814db8
Will Ozeas <willatme@me.com>
2023-04-10 17:54:42 -0400

    Mine ghosting VFX

--- Tanks In Advance/Assets/Scripts/Objects/Mine.cs
+++ Tanks In Advance/Assets/Scripts/Objects/Mine.cs
@@ -6,46 +6,78 @@ using UnityEngine.VFX;
 
 public class Mine : Bullet
 {
-    private bool exploding;
-    private MeshRenderer mesh;
-    private VisualEffect vfx;
+    [SerializeField] private Material blueMat;
+    [SerializeField] private Material pinkMat;
+    
     [SerializeField] private float explosionAnimTime = 0.5f;
     [SerializeField] private float radius = 3f;
+    private bool _exploding;
+    private MeshRenderer _mesh;
+    private VisualEffect _vfx;
+    private Color startColor;
+
+    private MaterialPropertyBlock _propBlock;
 
     public Mine(Tank source)
     {
         _tank = source;
-        _lifespan = 5f;
+        _currentLifespan = _totalLifespan;
 
-        exploding = false;
+        _exploding = false;
     }
     
     // Start is called before the first frame update
-    protected override void Start()
+    protected void Awake()
     {
-        _lifespan = 5f;
-        mesh = GetComponentInChildren<MeshRenderer>();
-        vfx = GetComponentInChildren<VisualEffect>();
+        _currentLifespan = _totalLifespan;
+        _mesh = GetComponentInChildren<MeshRenderer>();
+        _vfx = GetComponentInChildren<VisualEffect>();
+        _propBlock = new MaterialPropertyBlock();
+    }
+
+    public override void Init(Tank source, Vector2 angle)
+    {
+        if (source.Owner.PlayerNumber == PlayerNum.Player1)
+        {
+            _mesh.sharedMaterial = blueMat;
+        }
+        else
+        {
+            _mesh.sharedMaterial = pinkMat;
+        }
+        startColor = _mesh.sharedMaterial.GetColor("_EmissionColor");
+        base.Init(source, angle);
+    }
+
+    protected override void Ghostify()
+    {
+        base.Ghostify();
+        startColor = MaterialMod.ChangeHDRColorIntensity(startColor, -2f);
+        // MaterialMod.SetEmissiveColor(newColor, _mesh, _propBlock);
     }
 
     // FixedUpdate called every certain amt of time
     protected override void Update()
     {
-        if (_lifespan < 0)
+        if (_currentLifespan < 0)
         {
             Explode();
         }
-        _lifespan -= Time.deltaTime;
+        _currentLifespan -= Time.deltaTime;
+        // if (!is_ghost)
+        {
+            float intensityOffset = Mathf.Sin(220 * Mathf.Pow(_currentLifespan / _totalLifespan, 0.1f)) - 1.5f;
+            Color newColor = MaterialMod.ChangeHDRColorIntensity(startColor, intensityOffset);
+            MaterialMod.SetEmissiveColor(newColor, _mesh, _propBlock);
+        }
     }
 
     public void Explode()
     {
-        if (exploding)
+        if (_exploding)
             return;
 
-        Debug.Log("Explode mine");
-
-        exploding = true;
+        _exploding = true;
 
         if (!is_ghost)
         {
@@ -89,14 +121,14 @@ public class Mine : Bullet
 
     private void ExplosionAnim()
     {
-        mesh.enabled = false;
+        _mesh.enabled = false;
         KillSelf(explosionAnimTime);
-        vfx.SetFloat("ExplosionTime", explosionAnimTime);
-        vfx.SetFloat("ExplosionSize", radius);
+        _vfx.SetFloat("ExplosionTime", explosionAnimTime);
+        _vfx.SetFloat("ExplosionSize", radius);
         bool isBlue = _tank.Owner.PlayerNumber == PlayerNum.Player1;
-        vfx.SetInt("IsBlue", isBlue ? 0 : 1); //this is reversed but don't worry about it
-        vfx.SetInt("IsGhost", is_ghost ? 1 : 0);
-        vfx.Play();
+        _vfx.SetInt("IsBlue", isBlue ? 0 : 1); //this is reversed but don't worry about it
+        _vfx.SetInt("IsGhost", is_ghost ? 1 : 0);
+        _vfx.Play();
     }
 
     protected void OnTriggerEnter(Collider collision)
