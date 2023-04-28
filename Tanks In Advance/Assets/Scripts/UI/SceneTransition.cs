bef7c6d5ad1b9e547c02f36d87735179579a4be5
amao <amm28y@gmail.com>
2023-04-27 22:36:14 -0400

    added pause menu

--- Tanks In Advance/Assets/Scripts/UI/SceneTransition.cs
+++ Tanks In Advance/Assets/Scripts/UI/SceneTransition.cs
@@ -2,12 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransition : MonoBehaviour
 {
+    [SerializeField] private GameObject pausePanel;
+    private bool paused = false;
+
     public void Transition(string scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+            TogglePause();
+    }
+
+    public void TogglePause()
+    {
+        if (!paused)
+        {
+            Time.timeScale = 0f;
+            pausePanel.SetActive(true);
+            paused = true;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            pausePanel.SetActive(false);
+            paused = false;
+        }
+
     }
 
     public void QuitGame()
