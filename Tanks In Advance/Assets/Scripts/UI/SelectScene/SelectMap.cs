f520a3a9eb86df864acd8b930f3f91b2905e9bfc
davidfan168 <davidfan168@hotmail.com>
2023-04-27 22:12:57 -0400

    add round select

--- Tanks In Advance/Assets/Scripts/UI/SelectScene/SelectMap.cs
+++ Tanks In Advance/Assets/Scripts/UI/SelectScene/SelectMap.cs
@@ -14,25 +14,40 @@ public class SelectMap : MonoBehaviour
     public TextMeshProUGUI curr_map_txt;
     public Image next_map;
     public TextMeshProUGUI next_map_txt;
+
+    public GameObject covering;
+    public GameObject selectedTxt;
     
     public List<MapSelectBox> mapSelectBoxes;
 
-    public string nextScene;
+
+    [SerializeField] private SelectSceneManager selectSceneManager;
+
+    public bool active;
+    public bool selected;
 
     private int selection = 0;
     private float duration = 0;
 
     void Start()
     {
+        active = true;
+        selected = false;
         UpdateSelectionUI();
     }
 
     void Update()
     {
+        if (!active)
+        {
+            return;
+        }
+        
         if (Input.GetButtonDown("P1_Fire") || Input.GetButtonDown("P2_Fire"))
         {
             DataManager.Instance().selectedMap = mapSelectBoxes[selection].name;
-            SceneManager.LoadScene(nextScene);
+            DataManager.Instance().selectedMapName = mapSelectBoxes[selection].mapName;
+            Select();
             return;
         }
         
@@ -57,6 +72,26 @@ public class SelectMap : MonoBehaviour
             duration = 0;
         }
     }
+    
+    public void Activate()
+    {
+        active = true;
+    }
+
+    public void Select()
+    {
+        selectSceneManager.OnMapSelected();
+        active = false;
+        selected = true;
+        UpdateSelectionUI();
+    }
+
+    public void Unselect()
+    {
+        active = true;
+        selected = false;
+        UpdateSelectionUI();
+    }
 
     private void UpdateSelectionUI()
     {
@@ -73,5 +108,23 @@ public class SelectMap : MonoBehaviour
         MapSelectBox next_box = mapSelectBoxes[next_id];
         next_map.sprite = next_box.mapSprite;
         next_map_txt.text = next_box.mapName;
+
+        if (active)
+        {
+            covering.SetActive(false);
+        }
+        else
+        {
+            covering.SetActive(true);
+        }
+
+        if (selected)
+        {
+            selectedTxt.SetActive(true);
+        }
+        else
+        {
+            selectedTxt.SetActive(false);
+        }
     }
 }
---
+++ Tanks In Advance/Assets/Scripts/UI/SelectScene/SelectSceneManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectSceneManager : MonoBehaviour
+{
+    enum SelectState
+    {
+        NOTHING,
+        MAP_SELECTED,
+        ROUND_SELECTED,
+    }
+
+    private SelectState selectState;
+    [SerializeField] private SelectMap selectMap;
+    [SerializeField] private SelectRound selectRound;
+    [SerializeField] private GameObject confirmSelection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        confirmSelection.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            confirmSelection.SetActive(false);
+            switch (selectState)
+            {
+                case SelectState.NOTHING:
+                    break;
+                case SelectState.MAP_SELECTED:
+                    selectMap.Unselect();
+                    selectRound.UnActivate();
+                    selectState = SelectState.NOTHING;
+                    break;
+                case SelectState.ROUND_SELECTED:
+                    selectRound.Activate();
+                    selectState = SelectState.MAP_SELECTED;
+                    break;
+            }
+        }
+
+        if (Input.GetButtonDown("P1_Fire") || Input.GetButtonDown("P2_Fire"))
+        {
+            if (selectState == SelectState.ROUND_SELECTED)
+            {
+                OnConfirmSelected();
+            }
+        }
+    }
+
+    public void OnMapSelected()
+    {
+        selectState = SelectState.MAP_SELECTED;
+        selectRound.Activate();
+    }
+
+    public void OnRoundSelected()
+    {
+        selectState = SelectState.ROUND_SELECTED;
+        confirmSelection.SetActive(true);
+    }
+
+    public void OnConfirmSelected()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+}
